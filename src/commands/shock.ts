9c687e03ec6312ba9b6bbcf9d7179683323d7164
porta <portalmaster137@gmail.com>
2024-09-29 21:59:46 +1000

    SHOCK HAAHHAHA

---
+++ src/commands/shock.ts
@@ -0,0 +1,36 @@
+import * as DJS from 'discord.js';
+import getPrismaClient from '../prismaSingleton';
+import axios from 'axios';
+import { PISHOCK_NAME } from '../../util/consts';
+
+export default async function shock(ev: DJS.CommandInteraction) {
+    const prisma = await getPrismaClient();
+
+    const arg_user = ev.options.get('user')?.user as DJS.User;
+    const arg_duration = ev.options.get('duration')?.value as number;
+    const arg_intensity = ev.options.get('intensity')?.value as number;
+
+    const target = await prisma.shockUser.findUnique({where: {discordId: arg_user.id}});
+    if (!target) {
+        await ev.reply({content: 'User not found in the database', ephemeral: true});
+        return;
+    }
+    if (!target.allowedUsers.includes(ev.user.id)) {
+        await ev.reply({content: 'You are not allowed to shock this user', ephemeral: true});
+        return;
+    }
+    
+    await ev.reply({content: `Shocking ${arg_user.username} for ${arg_duration} seconds with intensity ${arg_intensity}`});
+    const res = await axios.post('https://do.pishock.com/api/apioperate', {
+        Username: target.pishockUsername,
+        Apikey: target.pishockAPIKey,
+        Code: target.pishockCode,
+        Name: PISHOCK_NAME,
+        Op: 0,
+        Duration: arg_duration,
+        Intensity: arg_intensity
+    });
+
+    await ev.editReply({content: res.data});
+    return;
+}
