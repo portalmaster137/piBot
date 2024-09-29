e6b1865a5bdb85108b3fcba9cc960c2efdee47e0
porta <portalmaster137@gmail.com>
2024-09-29 19:21:32 +1000

    Refactor code to use Prisma client singleton and add logging
    - Add "start" script to package.json to run the application
    - Update entry.ts to include Discord.js and set up environment variables

--- src/entry.ts
+++ src/entry.ts
@@ -1,6 +1,29 @@
 import getPrismaClient from "./prismaSingleton";
 import pino from 'pino';
 import * as denv from 'dotenv';
+import * as DJS from 'discord.js';
+
 
 const logger = pino()
 
+if (!process.env.TOKEN) {
+    logger.error('Token not found in .env file');
+    process.exit(1);
+}
+if (!process.env.CLIENT_ID) {
+    logger.error('Client ID not found in .env file');
+    process.exit(1);
+}
+if (!process.env.GUILD_ID) {
+    logger.error('Guild ID not found in .env file');
+    process.exit(1);
+}
+logger.info('Environment variables loaded successfully');
+
+const Intents = [
+    DJS.GatewayIntentBits.Guilds,
+    DJS.GatewayIntentBits.GuildMessages,
+    DJS.GatewayIntentBits.MessageContent,
+]
+
+const CLIENT = new DJS.Client({intents: Intents});
